7aeb4709c51770667ad11c99ae3ed763e8197f3b
vinoddsce <vinod.dsce@gmail.com>
2019-08-13 22:42:46 +0530

    Arrow function

--- index.js
+++ index.js
@@ -1,15 +1,75 @@
-// Function Constructor
+// sayHello();
+// function sayHello() {
+//     console.log("Hello ");
+// }
 
-var Person = function (id, name) {
+
+// console.log(sayHi)
+// var sayHi = function () {
+//     console.log("Hi ");
+// }
+// console.log(sayHi);
+
+
+
+
+
+
+
+
+
+var display = function () {
+    console.log(this);
+}
+
+var Person = function (id, firstName, lastName, isMarried, yearOfBirth) {
     this.id = id;
-    this.name = name;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.isMarried = isMarried;
+    this.yearOfBirth = yearOfBirth;
     this.display = function () {
-        console.log("ID: ", this.id, ", Name: ", this.name);
+        console.log(this);
+    }
+    this.calculateAge = function (currentYear) {
+        var getAge = (year) => {
+            console.log(this);
+            return year - this.yearOfBirth;
+        }
+        console.log("Age: ", getAge(currentYear));
+        console.log(this);
+        // function getAge(year) {
+        //     console.log(this);
+        //     return year - this.yearOfBirth;
+        // }
     }
 }
 
-var vinod = new Person(1, "Vinod");
-var nisha = new Person(2, "Nisha");
+// Person.prototype.calculateAge = function (currentYear) {
+//     console.log("Age: ", (currentYear - this.yearOfBirth));
+// }
 
-vinod.display();
-nisha.display();
+var vinod = new Person(1, "Vinod", "Kumar", false, 1990);
+
+// window.display();
+// vinod.display();
+
+vinod.calculateAge(2019);
+
+
+
+
+// Arrow Function
+// var arr_fun_name = () => {
+// }
+
+// var sayHi = () => {
+//     console.log(this);
+// }
+// sayHi();
+
+
+// var sayHi = (name) => {
+//     console.log("Hi,", name);
+// }
+// sayHi("Vinod");
